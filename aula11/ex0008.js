2c3c83785f4e409b48684d659f931c1c9080c54a
xandyfilho <alexandre.pavinatto22@gmail.com>
2023-03-06 20:25:14 -0300

    aula 11

---
+++ aula11/ex0008.js
@@ -0,0 +1,7 @@
+var vel = 60
+console.log(`Sua velocidade é de ${vel}km/h.`)
+if( vel > 60){ //Condição simples
+    console.log(`Você ultrapassou o limite de velocidade.MULTADO!`
+    )
+}
+console.log(`Dirija sempre com cuidado!`)
---
+++ aula11/ex0009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'BR'){
+    console.log('Você é brasileiro')
+}else{
+    console.log('Você é estrangeiro')
+}
